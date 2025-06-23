fe342a1b09cc2d9250a48a50fe115e637226b213
Wreetbhas Pal <79754822+Wreetbhas@users.noreply.github.com>
2025-06-23 13:32:56 +0530

    Update watchLocation.js

--- watchLocation.js
+++ watchLocation.js
@@ -37,7 +37,7 @@ function showPosition(position) {
         locationTrackLayers.removeLayer(circleMarker);
     }
 
-    circleMarker = L.circle([lat, lon], 10, circleMarkerOptions);
+    circleMarker = L.circle([lat, lon], 5, circleMarkerOptions);
     locationTrackLayers.addLayer(circleMarker);
 
     map.panTo(new L.LatLng(lat, lon));
