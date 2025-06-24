25673672e75130998ae04d80e8f24d33cbd1c55d
Wreetbhas Pal <79754822+Wreetbhas@users.noreply.github.com>
2025-06-24 16:55:04 +0530

    Update watchLocation.js

--- watchLocation.js
+++ watchLocation.js
@@ -1,23 +1,13 @@
 var watchId;
 var locationTrackLayers;
 var circleMarker;
-var interactionStarted = -5001;
-var interactionEnded = 0;
-
-
+var isUserInteracting = false;
+var interactionTimeout;
 var circleMarkerOptions = {
     color: '#0008ff',
     fillColor: '#0008ff',
     fillOpacity: 10
 }
-
-map.on('mousedown touchstart', function () {
-    interactionStarted = Date.now();
-});
-map.on('mouseup touchend', function () {
-    interactionEnded = Date.now();
-});
-
 
 function watchLocation() {
     const options = {
@@ -25,16 +15,54 @@ function watchLocation() {
         maximumAge: 0,
         timeout: 30000
     };
-
     if (navigator.geolocation) {
         locationTrackLayers = L.layerGroup([]);
         locationTrackLayers.addTo(map);
+
+        // Set up interaction listeners
+        setupMapInteractionListeners();
+
         watchId = navigator.geolocation.watchPosition(showPosition, showError, options);
     }
     else {
         alert("Geolocation is not supported by this browser.");
     }
+}
 
+function setupMapInteractionListeners() {
+    // Listen for various user interaction events
+    map.on('dragstart', onUserInteractionStart);
+    map.on('zoomstart', onUserInteractionStart);
+    map.on('movestart', onUserInteractionStart);
+
+    // Listen for interaction end events
+    map.on('dragend', onUserInteractionEnd);
+    map.on('zoomend', onUserInteractionEnd);
+    map.on('moveend', onUserInteractionEnd);
+
+    // Also listen for touch events on mobile
+    map.on('touchstart', onUserInteractionStart);
+    map.on('touchend', onUserInteractionEnd);
+}
+
+function onUserInteractionStart() {
+    isUserInteracting = true;
+    // Clear any existing timeout
+    if (interactionTimeout) {
+        clearTimeout(interactionTimeout);
+    }
+}
+
+function onUserInteractionEnd() {
+    // Use a timeout to ensure all interaction events have finished
+    // This prevents rapid fire of interaction events from interfering
+    if (interactionTimeout) {
+        clearTimeout(interactionTimeout);
+    }
+
+    interactionTimeout = setTimeout(() => {
+        isUserInteracting = false;
+    }, 1000); // Wait 1 second after last interaction before allowing auto-pan
 }
 
 function showPosition(position) {
@@ -42,17 +70,15 @@ function showPosition(position) {
     lon = position.coords.longitude;
     accuracy = position.coords.accuracy;
 
+    // Update the marker regardless of interaction state
     if (circleMarker) {
         locationTrackLayers.removeLayer(circleMarker);
     }
-
     circleMarker = L.circle([lat, lon], 3, circleMarkerOptions);
     locationTrackLayers.addLayer(circleMarker);
 
-    var not_interacted_duration = Date.now() - interactionEnded;
-
-
-    if ((interactionEnded >= interactionStarted) && (not_interacted_duration > 5000)) {
+    // Only pan the map if user is not currently interacting
+    if (!isUserInteracting) {
         map.panTo(new L.LatLng(lat, lon));
     }
 }
